feac1b0dbd8fa6f4e66635c30bd41464ffbbf137
MML <76636013+MML4379@users.noreply.github.com>
2023-04-14 13:13:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const topnav = document.getElementById("topics");
+const content = document.querySelector("body");
+
+if (content.scrollHeight > "50%") {
+    topnav.style.background = "#262626DD";
+}
